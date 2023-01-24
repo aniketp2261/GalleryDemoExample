2953a350d0d756ca232c0936726840d781d535e6
Aniket Patil <aniketp2261@gmail.com>
2023-01-24 15:51:57 +0530

    Initial Commit

---
+++ GalleryDemoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GalleryDemoApp
+//
+//  Created by Aniket Patil on 24/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
